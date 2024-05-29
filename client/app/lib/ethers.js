6ced366032055479ef88b63362f104b8c52225b8
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-05-29 12:59:21 +0000

    frontend implementation

---
+++ client/app/lib/ethers.js
@@ -0,0 +1,14 @@
+const ethers = require('ethers')
+
+export function getEthersProvider() {
+  // Check if MetaMask is installed and enabled
+ 
+  if (typeof window !== 'undefined') {
+    // Use MetaMask's injected provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    return provider
+  } else {
+    // Fall back to a JSON-RPC provider
+    return new ethers.providers.JsonRpcProvider('https://sepolia.era.zksync.dev');
+  }
+}
