018f7dd35e9e68bcefd6868687fd0883c393effa
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-07-19 20:52:22 +0000

    improve security and gas optimization

--- contracts/SocialDapp.sol
+++ contracts/SocialDapp.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SocialDapp {
-    address public owner;
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract SocialDapp is ReentrancyGuard, Ownable {
+    /**
+     * @dev Struct representing a user in the dApp.
+     * @param username The username of the user.
+     * @param userAddress The address of the user.
+     * @param isRegistered A boolean indicating if the user is registered.
+     */
     struct User {
         string username;
         address userAddress;
@@ -12,14 +19,30 @@ contract SocialDapp {
 
     mapping(address => User) public users;
 
+    /**
+     * @dev Struct representing a post in the dApp.
+     * @param author The address of the user who created the post.
+     * @param content The content of the post.
+     * @param timestamp The time when the post was created.
+     * @param likes The number of likes the post has received.
+     * @param commentsCount The number of comments on the post.
+     * @param likedBy A mapping to track if a user has liked the post.
+     */
     struct Post {
         address author;
         string content;
         uint256 timestamp;
         uint256 likes;
         uint256 commentsCount;
+        mapping(address => bool) likedBy; // Track likes by user
     }
 
+    /**
+     * @dev Struct representing a comment on a post.
+     * @param commenter The address of the user who made the comment.
+     * @param content The content of the comment.
+     * @param timestamp The time when the comment was made.
+     */
     struct Comment {
         address commenter;
         string content;
@@ -36,20 +59,22 @@ contract SocialDapp {
     event PostLiked(address indexed liker, uint256 indexed postId);
     event CommentAdded(address indexed commenter, uint256 indexed postId, string content, uint256 timestamp);
 
+    /**
+     * @dev Modifier to check if the sender is a registered user.
+     */
     modifier onlyRegisteredUser() {
         require(users[msg.sender].isRegistered, "User is not registered");
         _;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
-    }
-
     constructor() {
-        owner = msg.sender;
+        transferOwnership(msg.sender);
     }
 
+    /**
+     * @dev Registers a new user.
+     * @param _username The username chosen by the user.
+     */
     function registerUser(string memory _username) external {
         require(!users[msg.sender].isRegistered, "User is already registered");
         require(bytes(_username).length > 0, "Username should not be empty");
@@ -63,36 +88,53 @@ contract SocialDapp {
         emit UserRegistered(msg.sender, _username);
     }
 
-    
+    /**
+     * @dev Returns the user details for a given address.
+     * @param _userAddress The address of the user.
+     * @return The User struct for the given address.
+     */
     function getUserByAddress(address _userAddress) external view returns (User memory) {
         require(users[_userAddress].isRegistered, "User not found");
         return users[_userAddress];
     }
 
+    /**
+     * @dev Creates a new post.
+     * @param _content The content of the post.
+     */
     function createPost(string memory _content) external onlyRegisteredUser {
         require(bytes(_content).length > 0, "Content should not be empty");
 
-        posts.push(Post({
-            author: msg.sender,
-            content: _content,
-            timestamp: block.timestamp,
-            likes: 0,
-            commentsCount: 0
-        }));
+        Post storage newPost = posts.push();
+        newPost.author = msg.sender;
+        newPost.content = _content;
+        newPost.timestamp = block.timestamp;
 
         emit PostCreated(msg.sender, _content, block.timestamp);
     }
 
-    function likePost(uint256 _postId) external onlyRegisteredUser {
+    /**
+     * @dev Likes a post.
+     * @param _postId The ID of the post to like.
+     */
+    function likePost(uint256 _postId) external onlyRegisteredUser nonReentrant {
         require(_postId < posts.length, "Post does not exist");
 
         Post storage post = posts[_postId];
+        require(!post.likedBy[msg.sender], "User has already liked this post");
+
         post.likes++;
+        post.likedBy[msg.sender] = true;
 
         emit PostLiked(msg.sender, _postId);
     }
 
-    function addComment(uint256 _postId, string memory _content) external onlyRegisteredUser {
+    /**
+     * @dev Adds a comment to a post.
+     * @param _postId The ID of the post to comment on.
+     * @param _content The content of the comment.
+     */
+    function addComment(uint256 _postId, string memory _content) external onlyRegisteredUser nonReentrant {
         require(_postId < posts.length, "Post does not exist");
         require(bytes(_content).length > 0, "Comment should not be empty");
 
@@ -109,10 +151,23 @@ contract SocialDapp {
         emit CommentAdded(msg.sender, _postId, _content, block.timestamp);
     }
 
+    /**
+     * @dev Returns the total number of posts.
+     * @return The total number of posts.
+     */
     function getPostsCount() external view returns (uint256) {
         return posts.length;
     }
 
+    /**
+     * @dev Returns the details of a post.
+     * @param _postId The ID of the post.
+     * @return author The address of the post's author.
+     * @return content The content of the post.
+     * @return timestamp The time when the post was created.
+     * @return likes The number of likes the post has received.
+     * @return commentsCount The number of comments on the post.
+     */
     function getPost(uint256 _postId) external view returns (
         address author,
         string memory content,
@@ -121,10 +176,18 @@ contract SocialDapp {
         uint256 commentsCount
     ) {
         require(_postId < posts.length, "Post does not exist");
-        Post memory post = posts[_postId];
+        Post storage post = posts[_postId];
         return (post.author, post.content, post.timestamp, post.likes, post.commentsCount);
     }
 
+    /**
+     * @dev Returns the details of a comment on a post.
+     * @param _postId The ID of the post.
+     * @param _commentId The ID of the comment.
+     * @return commenter The address of the user who made the comment.
+     * @return content The content of the comment.
+     * @return timestamp The time when the comment was made.
+     */
     function getComment(uint256 _postId, uint256 _commentId) external view returns (
         address commenter,
         string memory content,
